0463d00c2bb1b6f5990934c3b5b3363785698b18
jraldGITHUB <jraldybaco@gmail.com>
2024-11-12 11:18:38 +0800

    added initTileLayer()

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ class EcoTech{
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
+    initTileLayer() {
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample for new corales BSIT student'
+        }).addTo(this.map);
+    }
 }
